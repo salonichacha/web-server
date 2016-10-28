80404875b3ead3b280e9b662ee087f04c7644d1f
Saloni Chacha <salonichacha92@gmail.com>
2016-10-27 20:52:13 -0700

    Initialize Repo

---
+++ script.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication : function(request, response, next){
+		console.log('Private Route hit');
+		next();
+	},
+
+	logger: function(request, response, next){
+		console.log('Request : ' request.method +' '+ request.originalUrl +' '+ new Date());
+		next();
+	}
+}
+
+app.use(middleware.logger);
+
+app.get('/', middleware.requireAuthentication , function(req, res){
+	res.send('Hello Express !');
+});
+
+
+app.get('/about',function(req, res){
+	res.send('Hello About !');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function(){
+	console.log('Express server started ' + PORT);
+});
